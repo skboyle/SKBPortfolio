87e2a48da5de152854a764bfb4879dc5f6fead68
Steve <skboylework@gmail.com>
2024-01-03 13:12:14 -0300

    fix typos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
       <h1>Steven Boyle</h1>
       <h3>Hi! I'm a software engineer with 6 years of professional experience. I have experience building RESTful APIs, Test Driven Development, End-to-End Testing, Micro-Service Infulstructure, E-Commerce Integration, Cloud Based Services, Database Updates, Monitoring, Continuous Deployment, Agile.</h3> 
       <h3>When I'm not building websites I like to paint, illustrate, make music, cook and ride my bike!</h3> 
-      <h3>You can visit my LinkedIn and Github, download my resume, or send me an email by using the icons at the bottom of the page. </h3>
+      <h3>You can visit my LinkedIn, Github, and resume, or send me an email by using the icons at the bottom of the page. </h3>
       <img src="https://i.imgur.com/BnV5nqG.jpgg" alt="laptop" width="600px"/>
     </div>
   );
--- src/components/FeedbackLoop.tsx
+++ src/components/FeedbackLoop.tsx
@@ -7,7 +7,7 @@ const FeedbackLoop: React.FC = () => {
     <div className="page" id="feedback-loop">
       <h1>FeedBack Loop</h1>
       <h3>Feedback loop is a forum gives artists submit their songs and received detailed constructive feedback and recomendations with Spotify integration! Designed with an intentionally minimal and art forward aestetic.</h3>
-      <h3>Stack: Ruby on Rails, React.js, custom CSS.</h3>
+      <h3>Stack: Ruby on Rails, React.js, CSS, Onmiauth</h3>
       <div className="centered-link">
 
       <a className="gh-link" href="https://github.com/skboyle/Feedback-Loop" target="_blank" rel="noopener noreferrer">
